8359d37fa7a6482d819c002db2aec95d63d56789
MmPreegel <146537784+MmPreegel@users.noreply.github.com>
2023-10-20 09:15:04 +0300

    Add files via upload

---
+++ checkTemp.js
@@ -0,0 +1,8 @@
+checkTemp (temperatuur) 
+{if (temperatuur >=0 && temperature <=20)} 
+{return -1; // liiga kylm
+else if (temperatuur >= 21 &&
+    temperatuur <=40)}
+{return 0; // ideaalne temperatuur}
+else if (temperatuur >=41 && temperatuur <=60)
+{return 1; // liiga kuum
