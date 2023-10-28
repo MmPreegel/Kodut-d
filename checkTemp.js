89db8fd0a4098adee5796265ff7d9261c8b5669e
MmPreegel <146537784+MmPreegel@users.noreply.github.com>
2023-10-28 11:42:13 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,8 +1,10 @@
-checkTemp (temperatuur) 
-{if (temperatuur >=0 && temperature <=20)} 
-{return -1; // liiga kylm
-else if (temperatuur >= 21 &&
-    temperatuur <=40)}
-{return 0; // ideaalne temperatuur}
-else if (temperatuur >=41 && temperatuur <=60)
-{return 1; // liiga kuum}
+function checkTemp(temperatuur) {
+  if (temperatuur >= 0 && temperature <= 20)
+
+    return -1; // liiga kylm
+  else if (temperatuur >= 21 && temperatuur <= 40)
+
+    return 0; // ideaalne temperatuur
+  else if (temperatuur >= 41 && temperatuur <= 60)
+    return 1
+}; // liiga kuum 
